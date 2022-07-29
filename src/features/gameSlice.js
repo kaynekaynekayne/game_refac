182c87b977bbebf9c51714e0522032930972d6b9
myplacecoffee <74812757+myplacecoffee@users.noreply.github.com>
2022-07-29 17:37:56 +0900

    fetch game

--- src/features/gameSlice.js
+++ src/features/gameSlice.js
@@ -1,18 +1,53 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import axios from "axios";
 
-const initialStates={
+// const GAME_URL=`https://api.rawg.io/api/games?key=${process.env.REACT_APP_KEY}&dates=${new Date().getFullYear()-1},${new Date().toISOString().slice(0,10)}&ordering=-rating&page_size=10`;
+//짭
+const GAME_URL="https://jsonplaceholder.typicode.com/users"
+
+const initialState={
     popular:[],
     newGames:[],
-    upComing:[]
+    upComing:[],
+    loading:null,
+    error:"",
 };
+
+
+export const fetchGames=createAsyncThunk(
+    'games/fetchGames',
+    async()=>{
+        try{
+            const resp=await axios.get(GAME_URL);
+            // return resp.data.results;
+
+            //짭
+            return resp.data;
+        }catch(err){
+            alert(err.message);
+        }
+    }
+)
 
 const gameSlice=createSlice({
     name:"games",
-    initialState:{value:initialStates},
+    initialState,
     reducers:{
-        fetch_games:(state,action)=>{
-            state.value=state.value;
-        }
+        
+    },
+    extraReducers:(builder)=>{
+        builder
+        .addCase(fetchGames.pending, (state,action)=>{
+            state.loading=true;
+        })
+        .addCase(fetchGames.fulfilled, (state,action)=>{
+            state.loading=false;
+            state.popular=action.payload;
+        })
+        .addCase(fetchGames.rejected, (state,action)=>{
+            state.loading=false;
+            
+        })
     }
 })
 
