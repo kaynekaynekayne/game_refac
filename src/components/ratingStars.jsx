1d2a02de5b684905fdeeb4b0520be61e1bf1ce2a
kaynekaynekayne <theygoon@gmail.com>
2022-10-24 17:53:54 +0900

    add props

--- src/components/ratingStars.jsx
+++ src/components/ratingStars.jsx
@@ -1,15 +1,12 @@
 import React from 'react';
 import StarRatings from 'react-star-ratings';
-import { useSelector } from 'react-redux';
 
-const RatingStars = (props) => {
-
-    const {detailInfo}=useSelector(state=>state.detail);
+const RatingStars = ({rating, ratingTop}) => {
 
     return(
         <StarRatings 
-            rating={detailInfo.rating}
-            numberOfStars={detailInfo.rating_top}
+            rating={rating}
+            numberOfStars={ratingTop}
             starDimension="1.2rem"
             starRatedColor="rgb(250, 225, 112)"
             starEmptyColor="rgb(230, 229, 227)"
